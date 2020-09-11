1a07832d16b783597ee54749325eaf122ba98913
Dawid Pichen <varia@pichen.com>
2020-09-11 19:46:41 +0200

    Improved DB structure and added DB initialise script.

--- db_writer.py
+++ db_writer.py
@@ -15,7 +15,7 @@ class DatabaseWriter:
                                            ssl_cafile=kafka_config['ssl-ca-file'],
                                            ssl_certfile=kafka_config['ssl-cert-file'],
                                            ssl_keyfile=kafka_config['ssl-key-file'],
-                                           auto_offset_reset="earliest", enable_auto_commit=True,
+                                           auto_offset_reset='earliest', enable_auto_commit=True,
                                            group_id='dpp-group',
                                            value_deserializer=lambda obj: json.loads(obj.decode('utf-8')))
         except errors.NoBrokersAvailable:
@@ -23,57 +23,14 @@ class DatabaseWriter:
         self._db = psycopg2.connect(host=pgsql_config['server'], port=pgsql_config['port'], user=pgsql_config['login'],
                                     password=pgsql_config['password'], database=pgsql_config['db'])
         self._cursor = self._db.cursor()
+        print("Connected to DB.")
 
     def close_db_connection(self):
         if self._cursor:
             self._cursor.close()
         if self._db:
             self._db.close()
-        print("Closed connection")
-
-    def initialise_db_structure(self):
-        # First, make sure to get rid of possible existing DB structure in a proper order.
-        self._cursor.execute("ALTER TABLE checks DROP CONSTRAINT IF EXISTS sites_checks")  # Relationship
-        self._cursor.execute("ALTER TABLE checks DROP CONSTRAINT IF EXISTS PK_checks")  # Private key
-        self._cursor.execute("ALTER TABLE sites DROP CONSTRAINT IF EXISTS PK_sites")  # Private key
-        self._cursor.execute("DROP INDEX IF EXISTS url_regex_idx")  # Index
-        self._cursor.execute("DROP TABLE IF EXISTS sites")  # Drop master table
-        self._cursor.execute("DROP TABLE IF EXISTS checks")  # Drop child table
-
-        # At this point we are sure that nothing is left behind and we can create the main table (sites) which will
-        # automatically generate an ID for each pair of site configuration, i.e. URL and regular expression
-        # (the latter can not be NULL but can be an empty string).
-        self._cursor.execute("""
-        CREATE TABLE sites
-        (
-            siteid SERIAL NOT NULL,
-            url CHARACTER VARYING NOT NULL,
-            regexp CHARACTER VARYING NOT NULL,
-            dateadded TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
-        )""")
-        # To increase the speed of site ID lookups used by "checks" tables, let's add an index on 2 fields used
-        # to find out the right ID.
-        self._cursor.execute("CREATE UNIQUE INDEX url_regex_idx ON sites (regexp,url)")
-        # Adding a primary key will also automatically add an index on "siteid" field.
-        self._cursor.execute("ALTER TABLE sites ADD CONSTRAINT PK_sites PRIMARY KEY (siteid)")
-        # Create a child table ("checks") which will store the results of the checkups.
-        self._cursor.execute("""
-        CREATE TABLE checks
-        (
-            siteid INTEGER NOT NULL,
-            checkuptime TIMESTAMP WITH TIME ZONE NOT NULL,
-            code SMALLINT NOT NULL,
-            responsetime NUMERIC(6,3)
-        )""")
-        self._cursor.execute("ALTER TABLE checks ADD CONSTRAINT PK_checks PRIMARY KEY (siteid, checkuptime)")
-        # Let's create relationship between the 2 tables (add foreign key to "checks" table and set behaviour when the
-        # record from the master table gets deleted or updated (cascade, which automatically does same thing in the
-        # child table.
-        self._cursor.execute("""
-        ALTER TABLE checks ADD CONSTRAINT sites_checks FOREIGN KEY (siteid) REFERENCES sites (siteid) 
-        ON DELETE CASCADE ON UPDATE CASCADE
-        """)
-        self._db.commit()
+        print("Closed connection to DB.")
 
     def move_messages_to_db(self):
         for messageObject in self._consumer:
@@ -107,7 +64,6 @@ if __name__ == "__main__":
     pgsqlConfig = config_reader.read_configs_from_ini('configs/general.ini', 'PostgreSQL')
     dbWriter = DatabaseWriter(kafkaConfig, pgsqlConfig)
     try:
-        dbWriter.initialise_db_structure()
         dbWriter.move_messages_to_db()
     finally:
         dbWriter.close_db_connection()
---
+++ init_db.py
@@ -0,0 +1,59 @@
+from libs import config_reader
+import psycopg2
+
+dbConfig = config_reader.read_configs_from_ini('configs/general.ini', 'PostgreSQL')
+
+dbConnection = psycopg2.connect(host=dbConfig['server'], port=dbConfig['port'], user=dbConfig['login'],
+                                password=dbConfig['password'], database=dbConfig['db'])
+try:
+    dbCursor = dbConnection.cursor()
+    try:
+        # First, make sure to get rid of possible existing DB structure in a proper order.
+        print("Purging old structure if exists already...")
+        dbCursor.execute("ALTER TABLE checks DROP CONSTRAINT IF EXISTS sites_checks")  # Relationship
+        dbCursor.execute("ALTER TABLE checks DROP CONSTRAINT IF EXISTS PK_checks")  # Private key
+        dbCursor.execute("ALTER TABLE sites DROP CONSTRAINT IF EXISTS PK_sites")  # Private key
+        dbCursor.execute("DROP INDEX IF EXISTS url_regex_idx")  # Index
+        dbCursor.execute("DROP TABLE IF EXISTS sites")  # Drop master table
+        dbCursor.execute("DROP TABLE IF EXISTS checks")  # Drop child table
+        # At this point we are sure that nothing is left behind and we can create the main table (sites) which will
+        # automatically generate an ID for each pair of site configuration, i.e. URL and regular expression
+        # (the latter can not be NULL but can be an empty string).
+        print("Creating new structure...")
+        dbCursor.execute("""
+                CREATE TABLE sites
+                (            
+                    siteid SERIAL NOT NULL,
+                    url CHARACTER VARYING NOT NULL,
+                    regexp CHARACTER VARYING NOT NULL,
+                    dateadded TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+                )""")
+        # To increase the speed of site ID lookups used by "checks" tables, let's add an index on 2 fields used
+        # to find out the right ID.
+        dbCursor.execute("CREATE UNIQUE INDEX url_regex_idx ON sites (regexp,url)")
+        # Adding a primary key will also automatically add an index on "siteid" field.
+        dbCursor.execute("ALTER TABLE sites ADD CONSTRAINT PK_sites PRIMARY KEY (siteid)")
+        # Create a child table ("checks") which will store the results of the checkups.
+        dbCursor.execute("""
+                CREATE TABLE checks
+                (            
+                    siteid INTEGER NOT NULL,
+                    checkid BIGSERIAL NOT NULL,
+                    checkuptime TIMESTAMP WITH TIME ZONE NOT NULL,
+                    code SMALLINT NOT NULL,
+                    responsetime NUMERIC(6,3)
+                )""")
+        dbCursor.execute("ALTER TABLE checks ADD CONSTRAINT PK_checks PRIMARY KEY (siteid, checkid)")
+        # Let's create relationship between the 2 tables (add foreign key to "checks" table and set behaviour when the
+        # record from the master table gets deleted or updated (cascade, which automatically does same thing in the
+        # child table.
+        dbCursor.execute("""
+                ALTER TABLE checks ADD CONSTRAINT sites_checks FOREIGN KEY (siteid) REFERENCES sites (siteid) 
+                ON DELETE CASCADE ON UPDATE CASCADE
+                """)
+        dbConnection.commit()
+        print("Operation completed.")
+    finally:
+        dbCursor.close()
+finally:
+    dbConnection.close()
